59a128d1848edc59d24e9b05afbed799ad9104f5
Mangesh <bmangesh46@gmail.com>
2024-03-06 17:54:04 +0530

    Added navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href="/properties">link to properties</Link>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ app/properties/add/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const addProperties = () => {
+  return <div>addProperties</div>;
+};
+
+export default addProperties;
